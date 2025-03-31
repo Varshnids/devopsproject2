1b95944744869540310dd4dfc1c2da1e896611b7
Varshni D S <127740252+Varshnids@users.noreply.github.com>
2025-03-31 20:36:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     chmod 777 build.sh
     echo build
     docker build -t test .
-    docker login -u varshni057 -p dckr_pat_I5RoAvTKqBpUUxWDfTuAGMQi87Y
-    docker tag test varshni057/ecommerce
-    docker push varshni057/ecommerce
+    docker login -u varshni057 -p @Varshni@1407
+    docker tag test varshni057/sample
+    docker push varshni057/sample
